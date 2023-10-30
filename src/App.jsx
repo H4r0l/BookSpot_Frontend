e1afb6970c3f5e884534deba808834046e4b2e43
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-10-30 16:24:14 -0500

    Feat: Installations and Styles fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,64 @@
-import { DrawerWithNavigation } from './components/Drawer'
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { DrawerWithNavigation } from "./components/Drawer";
+import AuthModal from "./components/auth/AuthModal";
+import { selectAuth, setAuthenticated } from "./components/redux/AuthSlices";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Gallery from "./components/Gallery";
+
+function Home() {
+  return <div></div>;
+}
+
+function LoginButton({ onClick }) {
+  return <button onClick={onClick}>Sign In</button>;
+}
 
 function App() {
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector(selectAuth);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(setAuthenticated(token));
+    }
+  }, [dispatch]);
+
+  const handleLoginClick = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <>
-      <div className="bg-[#67909C] p-4 flex justify-items-center ">
+      <div className="flex-row center-block p-6">
         <DrawerWithNavigation />
+        {!isAuthenticated && <LoginButton onClick={handleLoginClick} />}
       </div>
-      <div className='flex justify-center'>
-        <Login />
+      <div className="flex justify-center">
+        {!isAuthenticated && <a href="#Login">Login</a>}
       </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Outlet />}>
+            <Route index element={<Home />} />
+            <Route path="component" element={<Gallery />} />
+          </Route>
+        </Routes>
+      </Router>
+      {showModal && <AuthModal closeModal={closeModal} />}
     </>
   );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { authSlice} from "./components/redux/AuthSlices.jsx";
 import { ThemeProvider } from '@material-tailwind/react'
 import App from './App.jsx'
 import './index.css'
 
+const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer, // Add the authSlice reducer
+  },
+});
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
