ebd0f6e10bbaeb45ac093373116f067acc561701
Harol <haroldurrea6@gmail.com>
2024-10-15 22:44:59 -0500

    Minor update

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    <>
+      <nav className="navbar navbar-toggle bg-dark d-flex align-content-between text-dark" id="">
+        <div className="badge text-color-primary text-wrap">
+          Navbar
+        </div>
+      </nav>
+
+    </>
+}
+
+export default Navbar
