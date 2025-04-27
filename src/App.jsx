233081c3f4030218c6aa2ed8bc4a3bdfde54950c
Har0l <haroldurrea6@gmail.com>
2025-04-26 23:31:59 -0500

    No progress still

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from "./Components/Navbar.jsx"
+import "../css/custom.css"
 
 function App() {
 
   return (
     <>
     <Navbar/>
-        <div className="flex flex-col gap-5 p-6">
-            Hoal
+        <div className="flex align-items-center text-center justify-center p-2 .custom-font">
+          <div className="">
+            <div>
+              <h1 className="text-3xl font-bold">La granja de _____</h1>
+            </div>
+          </div>
         </div>
       </>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+import "../scss/custom.scss";
+
 const Navbar = () => {
   return (
     <>
       <nav
-        className="navbar navbar-toggle bg-dark text-light fw-bold mx-auto p-2"
+        className="navbar navbar-toggle bg-dark text-light fw-bold mx-auto p-2 justify-content-left"
         id=""
       >
-        <li>First Item</li>
-        <li> Second Item</li>
+          <li>First Item</li>
       </nav>
+      
     </>
   );
 };
